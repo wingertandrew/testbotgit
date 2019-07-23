61476eae987132051198a06e8a16397e2c772d02
Andrew Wingert <wingert.andrew@gmail.com>
2019-07-23 03:29:06 -0400

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -9,7 +9,8 @@ const app = dialogflow({debug: true});
 admin.initializeApp();
 const db = admin.firestore();
 const collectionRef = db.collection('planets');
-//-----------
+
+//--------------------------------------------------------------------------------------------
 
 app.intent('ask_planet_intent', (conv, {planet}) => {
     const term = planet.toLowerCase();
@@ -19,7 +20,6 @@ app.intent('ask_planet_intent', (conv, {planet}) => {
             const {crew, arrival, departure, hotelconf, phone, position} = snapshot.data();
     conv.ask(`Here's your info ${crew}, you arrive ${arrival} and you depart ${departure}.
                 Hotel conformation is ${hotelconf}, postion is ${position} and phone is ${phone}.` 
-               // + `What else do you want to know?`
                );
    
    
@@ -28,6 +28,26 @@ console.log('error:', e);
 conv.close('Error, Not that smart yet. Ask for "tell me about (crewmember).');
     });
 });
+
+//------------------------------------------------------------------------------------------
+
+/* app.intent('crew_ask_specific', (conv, {planet}) => {
+    const term = planet.toLowerCase();
+    const termRef = collectionRef.doc(`${term}`);
+        return termRef.get()
+            .then((snapshot) => {
+            const {crew, phone} = snapshot.data();
+    conv.ask(`Here's your info ${crew}, and phone is ${phone}.` 
+               );
+   
+   
+        }).catch((e) => {
+console.log('error:', e);
+conv.close('Error 2, the individual one does not work.');
+    });
+}); */
+
+//------------------------------------------------------------------------------------------
 
 
 exports.actionsOracle = functions.https.onRequest(app);
