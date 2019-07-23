11a481e646e18ec93d332c0ca8cf05cc18a7f883
Andrew Wingert <wingert.andrew@gmail.com>
2019-07-23 01:47:49 -0400

    update

--- functions/index.js
+++ functions/index.js
@@ -10,8 +10,6 @@ admin.initializeApp();
 const db = admin.firestore();
 const collectionRef = db.collection('planets');
 //-----------
-const collectionRefCrew = db.collection('crew');
-
 
 app.intent('ask_planet_intent', (conv, {planet}) => {
     const term = planet.toLowerCase();
@@ -29,23 +27,6 @@ conv.close('Sorry, try again and tell me another planet.');
     });
 });
 
-console.log("Line 30")
-
-app.intent('UserAskCrew', (conv, {crew}) => {
-    const termCrew = crew.toLowerCase();
-    const termRefCrew = collectionRefCrew.doc(`${termCrew}`);
-        return termRefCrew.get()
-            .then((snapshot) => {
-            const {email, position} = snapshot.data();
-    conv.ask(`Here you go, ${email}, job is ${position}` +
-            `Do you want info on another crew member?`);
-   
-   
-        }).catch((e) => {
-console.log('error:', e);
-conv.close('This is an error.');
-    });
-});
 
 exports.actionsOracle = functions.https.onRequest(app);
 console.log("Line 49")
