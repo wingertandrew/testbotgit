3b6b976b99ca4d16446b6f94ac436fd23afddd15
Andrew Wingert <wingert.andrew@gmail.com>
2019-07-24 05:21:37 -0400

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -9,6 +9,7 @@ const app = dialogflow({debug: true});
 admin.initializeApp();
 const db = admin.firestore();
 const collectionRef = db.collection('planets');
+const collectionRefAddress = db.collection('addresses');
 
 //--------------------------------------------------------------------------------------------
 
@@ -31,13 +32,13 @@ conv.close('Error, Not that smart yet. Ask for "tell me about (crewmember).');
 
 //------------------------------------------------------------------------------------------
 
-/* app.intent('crew_ask_specific', (conv, {planet}) => {
-    const term = planet.toLowerCase();
-    const termRef = collectionRef.doc(`${term}`);
+ app.intent('UserAskHotel', (conv, {address}) => {
+    const term = address.toLowerCase();
+    const termRef = collectionRefAddress.doc(`${term}`);
         return termRef.get()
             .then((snapshot) => {
-            const {crew, phone} = snapshot.data();
-    conv.ask(`Here's your info ${crew}, and phone is ${phone}.` 
+            const {address} = snapshot.data();
+    conv.ask(`Here's your info ${address}.` 
                );
    
    
@@ -45,9 +46,10 @@ conv.close('Error, Not that smart yet. Ask for "tell me about (crewmember).');
 console.log('error:', e);
 conv.close('Error 2, the individual one does not work.');
     });
-}); */
+}); 
 
 //------------------------------------------------------------------------------------------
+
 
 
 exports.actionsOracle = functions.https.onRequest(app);
